08b9a41a378f7e31f8317929f9846e92d55711e0
Jumao-as1 <mark.jumaoas04@gmail.com>
2025-03-14 22:16:48 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const MainPage = () => {
+return <>
+<div className="min-h-screen flex justify-center items-center ">
+  <div className="text-center">
+    <h1 className="text-7xl font-bold text-center mb-3">Mark Lester Jumao-as</h1>
+    <p className="text-lg">BSIT - 3</p>
+
+    <div className="mt-6 space-x-7  ">
+      <Link href={'/products'} className="button">See all Products</Link>
+      <Link href={'/todos'} className="button">See all ToDos</Link>
+    </div>
+  </div>
+   </div>
+</>
+}
+
+export default MainPage;
